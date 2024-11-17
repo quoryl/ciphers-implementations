f714c58f2824fad701822d4a86a035c2818d5ba7
quoryl <tatiana.sula@gmail.com>
2024-11-17 20:12:57 +0100

    Improved algorithm efficiency

--- ModularArithmetic/EsponenziazioneVeloce/EsponenziazioneVeloce.py
+++ ModularArithmetic/EsponenziazioneVeloce/EsponenziazioneVeloce.py
@@ -2,18 +2,30 @@ import sys
 import argparse
 
 def binary_modular_exponentiation(a, b, m):
-    binary_representation = [int(c) for c in bin(b)[2:][::-1]]
-    indices = list(enumerate(binary_representation))
+    # this version is more readable but less efficient, it computes more results than needed
+    # binary_representation = [int(c) for c in bin(b)[2:][::-1]]
+    # indices = list(enumerate(binary_representation))
 
-    # a^b mod m
-    d = 1
-    for k, b_k in enumerate(binary_representation):
-        if b_k == 1:
-            # Calculate a^(2^k) % m (only for b_k == 1)
-            local_result = pow(a, 2**k, m)
-            d = (d * local_result) % m
+    # # a^b mod m
+    # d = 1
+    # for k, b_k in enumerate(binary_representation):
+    #     if b_k == 1:
+    #         # Calculate a^(2^k) % m (only for b_k == 1)
+    #         local_result = pow(a, 2**k, m)
+    #         d = (d * local_result) % m
 
-    return d
+    # return d
+
+    result = 1
+    a = a % m 
+    while b > 0:
+        if b % 2 == 1: # here we are checking if the last bit of b is 1, in that case we need to accumulate the result
+            result = (result * a) % m
+    
+        a = (a * a) % m # deals with doubling the exponent of a at each iteration  
+        b //= 2 # deals with the binary representation factor of b
+
+    return result
 
 def main():
     parser = argparse.ArgumentParser(description="Calculate (a^b) mod m using the binary modular exponentiation algorithm.")
