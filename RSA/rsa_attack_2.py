6a3f4f3ba0bf9020ff1413b1e9e8bab7b2bf3496
quoryl <tatiana.sula@gmail.com>
2024-12-10 16:33:39 +0100

    fixed decryption attack iteration nr issue

--- RSA/rsa_attack_2.py
+++ RSA/rsa_attack_2.py
@@ -3,41 +3,37 @@ import random
 import math
 import time
 
- # n rsa modulus, d private exponent, e public exponent
 def decryptionexp(n: int, d: int, e: int):
-    # e*d - 1 = 2^r * m, m is odd, r >= 1
-    sum_ed = e*d - 1
-
+    m = e * d - 1
     r = 0
-    m = e*d - 1
     while m % 2 == 0:
         m //= 2
         r += 1
-  
-    while True:        
-        x = random.randint(1, n-1)
+    iterations = 0
+    while True:
+        iterations += 1
+        x = random.randint(1, n - 1)
         if math.gcd(x, n) != 1:
-            return x, 0 # risultato banale
-        
-        x_0 = pow(x, m, n)
-        x_values = [x_0]
-        
-        for i in range(1, r + 1):
-            x_next = pow(x_values[-1], 2, n)
-            x_values.append(x_next)
+            return x
 
-        for j in range(1, len(x_values)):
-            if x_values[j] == 1 and x_values[j - 1] != n - 1:
-                factor = math.gcd(x_values[j - 1] + 1, n)
+        x_0 = pow(x, m, n)
+        if x_0 == 1:
+            continue
+
+        for _ in range(1, r + 1):
+            x_next = pow(x_0, 2, n)
+            if x_next == 1 and x_0 != n - 1:
+                factor = math.gcd(x_0 + 1, n)
                 if 1 < factor < n:
-                    return factor, j
+                    return factor, iterations
+                break
+            x_0 = x_next
 
         
 def test_rsa_decryption(nr_tests, nr_bits):
     iterations = nr_tests
     k = nr_bits
 
-    n_list = []
     exec_time_list = []
     algorithm_iterations_list = []
 
@@ -47,10 +43,9 @@ def test_rsa_decryption(nr_tests, nr_bits):
         p = rsa.generate_prime(k)
         q = rsa.generate_prime(k)
         e, d, n = rsa.generate_key(p, q)
-        n_list.append(n)
 
         start_time = time.perf_counter()
-        factor, j = decryptionexp(n, d, e)
+        _, j = decryptionexp(n, d, e)
         end_time = time.perf_counter()
 
         exec_time = end_time - start_time
@@ -67,7 +62,7 @@ def test_rsa_decryption(nr_tests, nr_bits):
     var_exec_time /= iterations
 
     # calculate average algorithm iterations
-    avg_algorithm_iterations = sum(algorithm_iterations_list) / iterations
+    avg_algorithm_iterations = sum(algorithm_iterations_list) / len(algorithm_iterations_list)
 
     return avg_algorithm_iterations, var_exec_time, avg_exec_time
 
